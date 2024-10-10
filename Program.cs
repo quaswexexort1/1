d550208242ecc7b9ec347cc1afa997d5c272e370
quaswexexort1 <coldzerokot@gmail.com>
2024-10-10 11:32:08 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+try{﻿
     double x = 0.5;  // Пример для значения x
     double y = 1.0;  // Пример для значения y
     double d = 2.4;  // Пример для значения d
@@ -6,7 +6,10 @@
 
     double R = (Math.Pow(Math.Cos(y), 2)) + (2.4 * d) / Math.Exp(y) + Math.Log(Math.Pow(Math.Sin(x), 2) + 6);
     Console.WriteLine("Значение R:" +R);
-
+}
+catch{
+     Console.WriteLine("Ошибка")
+    }
 
 
 
